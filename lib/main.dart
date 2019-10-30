fa01b616a3519f9caa57ca45cf930b185d134f16
sudond <sudond@naver.com>
2019-10-30 19:44:36 +0900

    final 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -12,67 +13,69 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
           backgroundColor: Colors.teal,
           body: SafeArea(
-            child: Column(children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/hynn2.jpg'),
-              ),
-              Text('HYNN',
-                  style: TextStyle(
-                      fontSize: 40.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Quicksand')),
-              Text('child developer',
-                  style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                      fontFamily: 'IndieFlower')),
-              Container(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.call,
-                      size: 40.0,
-                      color: Colors.black,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      '010-3027-3779',
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 50.0,
+                    backgroundImage: AssetImage('images/hynn2.jpg'),
+                  ),
+                  Text('HYNN',
                       style: TextStyle(
-                        color: Colors.yellow,
-                        fontSize: 20.0,
+                          fontSize: 40.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Quicksand')),
+                  Text('child developer',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontFamily: 'IndieFlower')),
+                  SizedBox(
+                    height: 20.0,
+                    width: 150.0,
+                    child: Divider(
+                      color: Colors.teal.shade200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.white,
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.call,
+                        size: 40.0,
+                        color: Colors.black,
                       ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      size: 40.0,
-                      color: Colors.black,
+                      title: Text(
+                        '010-3027-3779',
+                        style: TextStyle(
+                          color: Colors.yellow,
+                          fontSize: 20.0,
+                        ),
+                      ),
                     ),
-                    SizedBox(
-                      width: 10.0,
+                  ),
+                  Card(
+                    color: Colors.white,
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        size: 40.0,
+                        color: Colors.black,
+                      ),
+                      title: Text('sudond@naver.com',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.teal.shade200,
+                          )),
                     ),
-                    Text(
-                      'sudond@naver.com',
-                      style: TextStyle(fontSize: 20.0, color: Colors.yellow),
-                    )
-                  ],
-                ),
-              ),
-            ]),
+                  ),
+                ]),
           )),
     );
   }

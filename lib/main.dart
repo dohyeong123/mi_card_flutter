98aae145f0939b40d0dddb93f1abfeffbb375430
sudond <sudond@naver.com>
2019-10-30 17:57:32 +0900

    first commmit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,37 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Column(children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/hynn2.jpg'),
+              ),
+              Text('HYNN',
+                  style: TextStyle(
+                      fontSize: 40.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Quicksand')),
+              Text('child developer',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: 'IndieFlower')),
+            ]),
+          )),
+    );
+  }
 }
